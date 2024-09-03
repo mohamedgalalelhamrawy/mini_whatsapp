ebda868fdac297d956d6b8316baf0ad8e7fc4bdc
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-09-03 14:55:16 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WhatsApp());
+}
+
+class WhatsApp extends StatelessWidget {
+  const WhatsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Container(),
+    );
+  }
+}
